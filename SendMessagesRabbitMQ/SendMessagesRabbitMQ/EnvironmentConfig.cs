1676d9353e49eb0058f299b1597d83d484ea8cda
lunacae <125094860+lunacae@users.noreply.github.com>
2023-04-02 20:03:47 -0300

    Projeto inicial

---
+++ SendMessagesRabbitMQ/SendMessagesRabbitMQ/EnvironmentConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace ResendMessages
+{
+    public static class EnvironmentConfig
+    {
+        public static class Rabbit
+        {
+            public static readonly string Host = GetVariableValueOrDefault("Rabbit.Host", "localhost");
+
+            public static readonly int Port = Int32.Parse(GetVariableValueOrDefault("Rabbit.Port", "5672"));
+
+            public static readonly string VirtualHost = GetVariableValueOrDefault("Rabbit.VirtualHost", "VirtualHost");
+
+            public static readonly string UserName = GetVariableValueOrDefault("Rabbit.UserName", "user");
+
+            public static readonly string Password = GetVariableValueOrDefault("Rabbit.Password", "123");
+        }
+
+        public static readonly string queue = GetVariableValueOrDefault("queue", "queue");
+        public static readonly string System = GetVariableValueOrDefault("System", "SystemToBind");
+        private static string GetVariableValueOrDefault(string variableName, string defaultValue = "")
+        {
+            string value = Environment.GetEnvironmentVariable(variableName, EnvironmentVariableTarget.Process);
+
+            return value ?? defaultValue;
+        }
+
+    }
+}
---
+++ SendMessagesRabbitMQ/SendMessagesRabbitMQ/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ResendMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Showing rabbit env: \n" +
+                $"Username: {EnvironmentConfig.Rabbit.UserName} \n" +
+                $"Password: {EnvironmentConfig.Rabbit.Password} \n" +
+                $"HostName: {EnvironmentConfig.Rabbit.Host} \n" +
+                $"Port: {EnvironmentConfig.Rabbit.Port} \n" +
+                $"VirtualHost: {EnvironmentConfig.Rabbit.VirtualHost}");
+
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = EnvironmentConfig.Rabbit.UserName,
+                Password = EnvironmentConfig.Rabbit.Password,
+                HostName = EnvironmentConfig.Rabbit.Host,
+                Port = EnvironmentConfig.Rabbit.Port,
+                VirtualHost = EnvironmentConfig.Rabbit.VirtualHost
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var messages = ReadMessages();
+                    Console.WriteLine($"Encontradas {messages.Count} mensagens para enviar");
+                    Console.WriteLine($"Enviando para o sistena {EnvironmentConfig.System}");
+                    foreach (var message in messages)
+                    {
+                        if (channel.IsClosed)
+                            throw new Exception("Canal fechado");
+
+                        IBasicProperties properties = channel.CreateBasicProperties();
+                        properties.Headers = new Dictionary<string, object>();
+                        properties.Headers.Add(new KeyValuePair<string, object>("System", EnvironmentConfig.System));
+
+                        var body = JsonConvert.SerializeObject(message);
+
+                        channel.BasicPublish("orders.headers", "order-operation", properties, Encoding.UTF8.GetBytes(body));
+                        Thread.Sleep(TimeSpan.FromSeconds(3));
+                    }
+                }
+            }
+            Console.WriteLine("Mensagens enviadas");
+        }
+
+        public static List<Object> ReadMessages()
+        {
+            string currentPath = Directory.GetCurrentDirectory();
+            currentPath = currentPath.Replace("\\bin\\Debug\\net6.0", "");
+            string path = currentPath + Path.DirectorySeparatorChar + @"Messages.json";
+
+            string text = System.IO.File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<List<Object>>(text);
+
+        }
+    }
+}
+
