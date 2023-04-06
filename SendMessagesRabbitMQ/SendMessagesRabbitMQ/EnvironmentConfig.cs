784509887425bf570b8b1064a8ecf7a6ab6b1ad9
lunacae <125094860+lunacae@users.noreply.github.com>
2023-04-06 08:41:13 -0300

    Update EnvironmentConfig.cs
    Adjusted variable names

--- SendMessagesRabbitMQ/SendMessagesRabbitMQ/EnvironmentConfig.cs
+++ SendMessagesRabbitMQ/SendMessagesRabbitMQ/EnvironmentConfig.cs
@@ -1,4 +1,4 @@
-﻿namespace ResendMessages
+namespace ResendMessages
 {
     public static class EnvironmentConfig
     {
@@ -6,17 +6,16 @@
         {
             public static readonly string Host = GetVariableValueOrDefault("Rabbit.Host", "localhost");
 
-            public static readonly int Port = Int32.Parse(GetVariableValueOrDefault("Rabbit.Port", "5672"));
+            public static readonly int Port = int.Parse(GetVariableValueOrDefault("Rabbit.Port", "5672"));
 
             public static readonly string VirtualHost = GetVariableValueOrDefault("Rabbit.VirtualHost", "VirtualHost");
 
-            public static readonly string UserName = GetVariableValueOrDefault("Rabbit.UserName", "user");
+            public static readonly string UserName = GetVariableValueOrDefault("Rabbit.UserName", "rabbitUser");
 
-            public static readonly string Password = GetVariableValueOrDefault("Rabbit.Password", "123");
+            public static readonly string Password = GetVariableValueOrDefault("Rabbit.Password", "rabbitPassword");
         }
 
-        public static readonly string queue = GetVariableValueOrDefault("queue", "queue");
-        public static readonly string System = GetVariableValueOrDefault("System", "SystemToBind");
+        public static readonly string DemoArg = GetVariableValueOrDefault("Argument", "ArgName");
         private static string GetVariableValueOrDefault(string variableName, string defaultValue = "")
         {
             string value = Environment.GetEnvironmentVariable(variableName, EnvironmentVariableTarget.Process);
